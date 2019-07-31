327e32a3dec046f24b3699d896c94b921696290e
lester <mlab817@gmail.com>
2019-07-31 17:13:34 +0800

    Update axios.js

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+var token = localStorage.getItem('token')
+
 const axiosInstance = axios.create({
-	baseURL: process.env.NODE_ENV !== "production"
-	? "http://localhost:8000/api/v1"
-	: "http://afmp-api.daplanningcentral.net/api/v1"
+	baseURL: process.env.NODE_ENV !== "production" ? "http://localhost:8000/api/v1" : "http://afmp-api.daplanningcentral.net/api/v1",
+  timeout: 5000,
+  headers: {
+	  'Authorization': 'Bearer ' + token,
+    'Content-Type': 'application/json'
+  }
 });
 
 export default async ({ Vue }) => {
