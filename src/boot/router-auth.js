30c84d5c5895ebf42c3dfc2554b8d9e9bc9a123a
Lester <mlab817@gmail.com>
2019-08-02 08:27:49 +0800

    Added router-auth to redirect user based on login status

---
+++ src/boot/router-auth.js
@@ -0,0 +1,13 @@
+export default ({ router }) => {
+  router.beforeEach(( to, from, next ) => {
+    let loggedIn = localStorage.getItem('loggedIn');
+
+    if (!loggedIn && to.path !=='/login') {
+      next('login');
+    } else if (loggedIn && to.path == '/login') {
+      next('/');
+    } else {
+      next();
+    }
+  });
+}
