4c7882a942191ef89fbc141ac50886ff217c2aa9
lester <mlab817@gmail.com>
2019-08-05 09:03:17 +0800

    Update MyLayout.vue
    added notifications q-menu

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -36,7 +36,36 @@
             color="grey-6"
             round
             icon="notifications"
-            class="q-mr-xs"/>
+            class="q-mr-xs">
+            <q-menu :content-style="{ backgroundColor: '#eee', color: 'blue'}">
+              <q-list style="min-width: 100px">
+                <q-item clickable>
+                  <q-item-section>New tab</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>New incognito tab</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Recent tabs</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>History</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Downloads</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Settings</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Help &amp; Feedback</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
 
           <q-btn
             flat
