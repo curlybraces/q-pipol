dcd11bca09595d405a10de69a7bfa819244e4ca7
mlab817 <mlab817@gmail.com>
2019-08-13 18:53:56 +0800

    commit

---
+++ src/components/ViewProject/UpdateImplementationPeriod.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-dialog
+    v-model="dialog"
+    position="bottom"
+    square
+    persistent>
+    <q-card>
+      <q-card-section header>
+        <div class="row">
+          <div class="text-h6">
+            Implementation Period
+          </div>
+          <q-space/>
+          <q-btn round dense flat icon="close" @click="dialog = false"/>
+        </div>
+      </q-card-section>
+      <q-separator/>
+      <q-card-section>
+        <q-select
+          v-model="implementation_start">
+        </q-select>
+        <q-select
+          v-model="implementation_end">
+        </q-select>
+      </q-card-section>
+      <q-separator/>
+      <q-card-actions>
+        <q-space/>
+        <q-btn
+          label="cancel"
+          color="negative"
+          />
+        <q-btn
+          label="save"
+          color="primary"
+          />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+<template>
+
+<script>
+  export default {
+    name: "UpdateImplementationPeriod",
+    data() {
+      return {
+        implementation_start: "",
+        implementation_end: ""
+      }
+    }
+  }
+</script>
