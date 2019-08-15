8aa812e7fcdfd69fbe8bd03adaca71d28b9dc446
Lester <mlab817@gmail.com>
2019-08-15 21:20:56 +0800

    commit

---
+++ src/components/Notifications/NotificationsModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <q-item-label>Notifications</q-item-label>
+    </q-card-section>
+    <q-card-section>
+      
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+  export default {
+    name: "NotificationsModal"
+  }
+</script>
