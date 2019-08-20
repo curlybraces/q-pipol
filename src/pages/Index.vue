e60361c061b2845f0fdabb28bcdd3b5e39686418
mlab817 <mlab817@gmail.com>
2019-08-20 12:27:09 +0800

    Commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,32 @@
 <template>
   <q-page padding>
-    <p>Dashboard</p>
+    <p>Home</p>
 
     <welcome-dialog />
+
+      <q-card square>
+          <q-card-section class="bg-primary text-white text-uppercase" style="padding:5px">
+            <div class="text-h6">New Thinking in Agriculture</div>
+          </q-card-section>
+        <q-carousel
+          animated
+          infinite
+          v-model="slide"
+          autoplay
+          arrows
+          transition-prev="slide-right"
+          transition-next="slide-left"
+          class="q-mb-md">
+          <q-carousel-slide v-for="item in new_thinkings" :name="item.id" :img-src=" 'statics/' + item.image"
+                            :key="item.id">
+              <div class="absolute-bottom custom-caption">
+                  <div class="text-h6">{{ item.id + '. ' + item.name }}</div>
+                  <div class="text-caption">{{ item.description }}</div>
+              </div>
+          </q-carousel-slide>
+
+        </q-carousel>
+      </q-card>
 
     <div class="row q-col-gutter-sm">
       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -44,48 +68,49 @@
         </q-card>
       </div>
       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <q-card class="col bg-red" dark>
+          <q-card-section>
+            <q-item dense>
+              <q-item-section avatar>
+                <q-icon name="delete" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Total Projects</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <div class="text-h2">
+                  10
+                </div>
+              </q-item-section>
+            </q-item>
+          </q-card-section>
+        </q-card>
+      </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
         <q-card class="col bg-accent" dark>
-          <q-card-section>
-            <q-item dense>
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Total Projects</q-item-label>
-              </q-item-section>
-              <q-item-section side>
-                <div class="text-h2">
-                  10
-                </div>
-              </q-item-section>
-            </q-item>
-          </q-card-section>
+            <q-card-section>
+                <q-item dense>
+                    <q-item-section avatar>
+                        <q-icon name="notifications" />
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>Notifications</q-item-label>
+                    </q-item-section>
+                    <q-item-section side>
+                        <div class="text-h2">
+                            10
+                        </div>
+                    </q-item-section>
+                </q-item>
+            </q-card-section>
         </q-card>
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <q-card class="col bg-positive" dark>
-          <q-card-section>
-            <q-item dense>
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Total Projects</q-item-label>
-              </q-item-section>
-              <q-item-section side>
-                <div class="text-h2">
-                  10
-                </div>
-              </q-item-section>
-            </q-item>
-          </q-card-section>
-        </q-card>
-      </div>
+    </div>
     </div>
   </q-page>
 </template>
 
 <script>
+    import { mapState, mapActions } from "vuex";
 import WelcomeDialog from "../components/Dashboard/WelcomeDialog";
 
 export default {
@@ -95,8 +120,25 @@ export default {
   },
   data() {
     return {
+        slide: 1
     };
   },
-  mounted() {}
+  computed: {
+      ...mapState("new_thinkings",["new_thinkings"])
+  },
+  methods: {
+      ...mapActions("new_thinkings",["loadNewThinkings"])
+  },
+  mounted() {
+      this.loadNewThinkings();
+  }
 };
 </script>
+
+<style lang="stylus" scoped>
+  .custom-caption
+    text-align center
+    padding 12px
+    color white
+    background-color rgba(0, 0, 0, .6)
+</style>
