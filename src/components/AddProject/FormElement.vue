2857180e6a4a3d5d29606f216beaae98dca494be
Lester <mlab817@gmail.com>
2019-08-28 21:12:08 +0800

    changed form to list

--- src/components/AddProject/FormElement.vue
+++ src/components/AddProject/FormElement.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="row items-center q-my-md">
+  <!-- <div class="row items-center q-my-md">
     <label-component>{{ label }}</label-component>
     <div class="col-md-9 col-sm-12 col-xs-12">
       <slot></slot>
     </div>
-  </div>
-
+  </div> -->
+  <q-item
+    >
+    <q-item-section class="col-2 gt-sm">
+      <q-item-label>
+        <label-component>{{ label}}</label-component>
+      </q-item-label>
+    </q-item-section>
+    <q-item-section>
+      <slot></slot>
+    </q-item-section>
+  </q-item>
 </template>
 
 <script>
