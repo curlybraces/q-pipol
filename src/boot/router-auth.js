bf9dfb65c01ca8837b663ffba2f8c8bf31faa670
Mark Lester Bolotaolo <marklesterbolotaolo@Marks-iMac.local>
2019-12-19 17:52:19 +0800

    refactored router-auth

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,39 +1,11 @@
-import { LocalStorage, Dialog } from "quasar";
-import { firebaseAuth } from "boot/firebase";
+import { LocalStorage } from "quasar";
 
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
-    let loggedIn = LocalStorage.getItem("loggedIn");
-    // if (!loggedIn && to.path !== "/login") {
-    //   next("/login");
-    // } else if (loggedIn && to.path == "/login") {
-    //   next("/");
-    // } else if (loggedIn && to.path == "/add" && !verifiedUser) {
-    //   // check if user verified their email
-    //   let user = firebaseAuth.currentUser;
-    //   var verifiedUser = user.emailVerified;
-    //   if (!verifiedUser) {
-    //     Dialog.create({
-    //       message: "Please verify your email to create/add projects."
-    //     });
-    //   } else {
-    //     next();
-    //   }
-    // } else {
-    //   next();
-    // }
+    const loggedIn = LocalStorage.getItem("loggedIn");
     if (!loggedIn && to.path !== "/") {
+      alert("Please login to continue.");
       next("/");
-    } else if (loggedIn && to.path == "/add") {
-      let user = firebaseAuth.currentUser;
-      var verifiedUser = user.emailVerified;
-      if (!verifiedUser) {
-        Dialog.create({
-          message: "Please verify your email to create/add projects."
-        });
-      } else {
-        next();
-      }
     } else {
       next();
     }
