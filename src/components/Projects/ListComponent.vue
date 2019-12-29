f9993a2a287a496392f4d54082b77f3cb459cfe6
Mark Lester Bolotaolo <mlab817@gmail.com>
2019-12-29 09:15:31 +0800

    updated list component

--- src/components/Projects/ListComponent.vue
+++ src/components/Projects/ListComponent.vue
@@ -8,88 +8,17 @@
       <q-toolbar>
         <q-toolbar-title>Projects</q-toolbar-title>
       </q-toolbar>
-      <q-tabs
-        v-model="tab"
-        dense
-        class="text-grey"
-        active-color="primary"
-        indicator-color="primary"
-        align="justify"
-      >
-        <q-tab name="new" :label="'New (' + newProjects.length + ')'" />
-        <q-tab
-          name="encoded"
-          :label="'Encoded (' + encodedProjects.length + ')'"
-        />
-        <q-tab
-          name="reviewed"
-          :label="'Reviewed (' + reviewedProjects.length + ')'"
-        />
-        <q-tab
-          name="finalized"
-          :label="'Finalized (' + finalizedProjects.length + ')'"
-        />
-      </q-tabs>
-
-      <q-separator />
-
-      <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="new">
-          <q-list separator>
-            <template v-if="newProjects.length">
-              <project
-                :key="project.id"
-                v-for="project in newProjects"
-                id="project.id"
-                v-bind="project"
-              ></project>
-            </template>
-            <no-project v-else></no-project>
-          </q-list>
-        </q-tab-panel>
-
-        <q-tab-panel name="encoded">
-          <q-list separator>
-            <template v-if="encodedProjects.length">
-              <project
-                v-for="project in encodedProjects"
-                :key="project.id"
-                id="project.id"
-                v-bind="project"
-              ></project>
-            </template>
-            <no-project v-else></no-project>
-          </q-list>
-        </q-tab-panel>
-
-        <q-tab-panel name="reviewed">
-          <q-list separator>
-            <template v-if="reviewedProjects.length">
-              <project
-                v-for="project in reviewedProjects"
-                :key="project.id"
-                id="project.id"
-                v-bind="project"
-              ></project>
-            </template>
-            <no-project v-else></no-project>
-          </q-list>
-        </q-tab-panel>
-
-        <q-tab-panel name="finalized">
-          <q-list separator>
-            <template v-if="finalizedProjects.length">
-              <project
-                v-for="project in finalizedProjects"
-                :key="project.id"
-                id="project.id"
-                v-bind="project"
-              ></project>
-            </template>
-            <no-project v-else></no-project>
-          </q-list>
-        </q-tab-panel>
-      </q-tab-panels>
+      <q-list separator>
+        <template v-if="finalizedProjects.length">
+          <project
+            v-for="project in finalizedProjects"
+            :key="project.id"
+            id="project.id"
+            v-bind="project"
+          ></project>
+        </template>
+        <no-project v-else></no-project>
+      </q-list>
     </q-card>
   </transition>
 </template>
