5807a9aee4b179fbc34936bd0f5eebdba4c0ba61
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-02-07 13:15:45 +0800

    edited create-project

--- src/pages/CreateProject.vue
+++ src/pages/CreateProject.vue
@@ -2,54 +2,58 @@
   <q-page padding>
     <page-breadcrumbs :breadcrumbs="breadcrumbs" />
 
-    <div>
-      <q-stepper
-        v-model="step"
-        ref="stepper"
-        animated
-        active-color="primary"
-        header-nav
-        :contracted="contracted"
-        flat
-      >
-        <q-step :name="1" prefix="1" title="General Information">
-          <div class="text-h6 q-mb-md">General Information</div>
-          <q-input outlined label="Project Title"></q-input>
-        </q-step>
-
-        <q-step :name="2" prefix="2" title="Infrastructure Information">
-          <div class="column q-gutter-y-md">
-            <q-card square>Target</q-card>
-            <q-card square>Accomplishments</q-card>
-          </div>
-        </q-step>
-
-        <q-step :name="3" prefix="3" title="Investment Information">
-          <div class="column q-gutter-y-md">
-            <q-card square>Target</q-card>
-            <q-card square>Accomplishments</q-card>
-          </div>
-        </q-step>
-
-        <template v-slot:navigation>
-          <q-stepper-navigation>
-            <q-btn
-              @click="$refs.stepper.next()"
-              color="deep-orange"
-              :label="step === 3 ? 'Finish' : 'Continue'"
-            />
-            <q-btn
-              v-if="step > 1"
-              flat
-              color="deep-orange"
-              @click="$refs.stepper.previous()"
-              label="Back"
-              class="q-ml-sm"
-            />
-          </q-stepper-navigation>
-        </template>
-      </q-stepper>
-    </div>
+    <q-card class="bg-white">
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          expand-separator
+          icon="info"
+          label="General Information"
+          caption="John Doe"
+          default-opened
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
+              dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+        <q-expansion-item
+          expand-separator
+          icon="attach_money"
+          label="Financial Information"
+          caption="John Doe"
+          default-opened
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
+              dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+        <q-expansion-item
+          expand-separator
+          icon="system_update_alt"
+          label="Updates"
+          caption="John Doe"
+          default-opened
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
+              dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </q-list>
+    </q-card>
   </q-page>
 </template>
 
@@ -77,8 +81,7 @@ export default {
         {
           title: "Create Project"
         }
-      ],
-      step: 1
+      ]
     };
   }
 };
