c5b8ecbb3c5e424ee0465aa30e77542c4548ca8f
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-02-17 16:56:14 +0800

    add

---
+++ src/components/FormInputs/NumberInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col">
+    <q-input
+      v-model="model"
+      :label="label"
+      stack-label
+      :dense="dense"
+      outlined
+      @input="val => $emit('input', val)"
+      :readonly="readonly"
+      :rules="rules"
+      :hint="hint"
+      clearable
+      type="number"
+      :suffix="suffix"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: ["label", "dense", "value", "readonly", "rules", "hint","suffix"],
+  data() {
+    return {
+      model: null
+    };
+  }
+};
+</script>
+
+<style>
+input[type=number]::-webkit-inner-spin-button, 
+input[type=number]::-webkit-outer-spin-button { 
+  -webkit-appearance: none; 
+  margin: 0; 
+}
+</style>
