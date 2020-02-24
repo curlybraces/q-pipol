78064658760073a303f8179cad5c00ebe4ccc7d3
Mark Lester Bolotaolo <marklesterbolotaolo@Marks-iMac.local>
2020-02-24 12:34:35 +0800

    created file for stepper navigation

---
+++ src/components/Projects/StepperNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-stepper-navigation>
+    <q-btn
+      @click="$emit('next')"
+      color="orange-10"
+      label="Next"
+      class="q-ml-sm"
+    />
+    <q-btn
+      flat
+      @click="$emit('back')"
+      color="orange-5"
+      label="Back"
+      class="q-ml-sm"
+      v-if="withBack"
+    />
+  </q-stepper-navigation>
+</template>
+
+<script>
+export default {
+  name: "StepperNavigation",
+  props: ["withBack"]
+};
+</script>
