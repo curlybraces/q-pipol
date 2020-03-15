dc369d7022f7723780d9be623d2d88ec66b3bd33
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-03-15 15:24:49 +0800

    made dropdown beautiful

--- src/components/layout/Dropdown.vue
+++ src/components/layout/Dropdown.vue
@@ -1,38 +1,59 @@
 <template>
-  <q-menu max-width="300px">
-    <q-list style="min-width: 280px">
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <q-img
-              :src="
+  <q-menu max-width="300px" :offset="[0,15]">
+    <div class="q-pa-md q-gutter-y-sm text-center">
+      <q-avatar>
+        <q-img
+          :src="
                 image
                   ? 'statics/agency_logos/' + image
                   : 'statics/avatar-placeholder.png'
               "
-            />
+        />
+      </q-avatar>
+      <q-item-label>{{ name }}</q-item-label>
+      <q-item-label caption>{{ email }}</q-item-label>
+    </div>
+    <q-separator/>
+    <q-list style="min-width: 280px" separator dense>
+      <q-item clickable v-close-popup to="/account">
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon name="tune" color="primary"/>
           </q-avatar>
         </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ name }}</q-item-label>
-          <q-item-label caption>{{ email }}</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-separator />
-      <q-item clickable v-close-popup to="/account">
-        <q-item-section>Account</q-item-section>
+        <q-item-section>Account Settings</q-item-section>
       </q-item>
       <q-item clickable v-close-popup to="/activity">
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon name="work_outline" color="primary"/>
+          </q-avatar>
+        </q-item-section>
         <q-item-section>Activity</q-item-section>
       </q-item>
       <q-item clickable v-close-popup to="/settings">
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon name="settings" color="primary"/>
+          </q-avatar>
+        </q-item-section>
         <q-item-section>Settings</q-item-section>
       </q-item>
       <q-separator />
       <q-item clickable v-close-popup>
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon name="contact_support" color="primary"/>
+          </q-avatar>
+        </q-item-section>
         <q-item-section>Help &amp; Feedback</q-item-section>
       </q-item>
       <q-item clickable v-close-popup @click="showLogout">
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon name="exit_to_app" color="primary"/>
+          </q-avatar>
+        </q-item-section>
         <q-item-section>Logout</q-item-section>
       </q-item>
     </q-list>
