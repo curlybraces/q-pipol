10a4a406705953911e80bedc0f8682024bd21170
mlab817 <mlab817@gmail.com>
2020-03-24 13:22:24 +0800

    added page title component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-toolbar class="q-mt-lg">
+
+    <q-item-label header class="q-pl-none">
+      {{ title }}
+    </q-item-label>
+
+    <q-space />
+
+    <slot></slot>
+
+  </q-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: ["title"]
+}
+</script>
