4dab7372face26d6a7a29ce80e8c08a24eed6f0c
mlab817 <mlab817@gmail.com>
2020-03-26 10:15:40 +0800

    moved notification into its own component

--- src/components/layout/RightDrawer.vue
+++ src/components/layout/RightDrawer.vue
@@ -1,40 +1,27 @@
 <template>
   <q-list separator>
-    <q-item-label header class="text-weight-bolder text-black text-h6"
-      >Notifications</q-item-label
-    >
-    <q-item
-      v-for="(notification, key) in notifications"
-      :key="key"
-    >
-      <q-item-section avatar>
-        <q-avatar color="primary" class="text-white">{{
-          notification.data.from.charAt(0)
-        }}</q-avatar>
-      </q-item-section>
-      <q-item-section>
-        <q-item-label class="text-weight-bolder text-uppercase">{{
-          notification.data.from
-        }}</q-item-label>
-        <q-item-label lines="2">{{ notification.data.title }}</q-item-label>
-        <q-item-label caption>{{ notification.created_at }}</q-item-label>
-      </q-item-section>
-    </q-item>
+    <div class="text-weight-bold q-pa-sm">
+      Notifications
+    </div>
+    <q-separator/>
+    <template v-for="(notification, key) in notifications">
+      <notification-item :notification="notification" :key="key"></notification-item>
+    </template>
   </q-list>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import NotificationItem from "../Notifications/NotificationItem";
 
 export default {
   name: 'RightDrawer',
-  data() {
-    return {
-      // notifications: []
-    };
-  },
-  computed: {
-    ...mapState('notifications',['notifications'])
+    components: {NotificationItem},
+    computed: {
+    ...mapState('notifications',['notifications']),
+    message() {
+      return '';
+    }
   },
   methods: {
     ...mapActions('notifications',['fetchNotifications'])
