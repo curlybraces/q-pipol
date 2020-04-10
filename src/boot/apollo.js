449ea06fccaf69a7a3aff3fc0a60c2c465295c11
mlab817 <mlab817@gmail.com>
2020-04-10 23:57:09 +0800

    successfully reconfigured apollo to use apollo-boost only

--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -1,10 +1,4 @@
-// Basic Installation: https://vue-apollo.netlify.com/guide/installation.html#_1-apollo-client
-// Pusher Integration: https://medium.com/sjk5766/laravel-graphql-subscription-vue-apollo-or-nuxt-338108ffb1cb
-// Hasura reference: https://hasura.io/learn/graphql/vue/apollo-client/
-
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
 import { LocalStorage } from 'quasar';
@@ -14,29 +8,37 @@ const uri = process.env.DEV
   ? 'https://da-ipms.herokuapp.com/graphql'
   : 'https://da-ipms.herokuapp.com/graphql';
 
-const httpLink = new HttpLink({
-  uri: uri
-});
-
 const cache = new InMemoryCache({
-  addTypename: false
-});
-
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-  const token = LocalStorage.getItem('token');
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : null
-    }
-  });
-  return forward(operation);
+  addTypename: true
 });
 
 export const apolloClient = new ApolloClient({
-  link: authMiddleware.concat(httpLink),
+  uri: uri,
   cache,
-  connectToDevTools: true
+  connectToDevTools: true,
+  fetchOptions: {
+    credentials: 'include'
+  },
+  request: operation => {
+    const token = LocalStorage.getItem('token') || '';
+    operation.setContext({
+      headers: {
+        authorization: 'Bearer ' + token
+      }
+    });
+  },
+  onError: ({ graphQLErrors, networkError }) => {
+    if (networkError) {
+      console.log('[networkError: ', networkError);
+    }
+
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        console.log('graphQLErrors: ');
+        console.dir(err);
+      }
+    }
+  }
 });
 
 export const apolloProvider = new VueApollo({
