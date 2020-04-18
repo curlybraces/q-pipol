c3f9d1d9f5a9ade1a59c3b33125f38804cc05a11
mlab817 <mlab817@gmail.com>
2020-04-18 22:23:57 +0800

    created first service - projects

---
+++ src/services/projects.js
@@ -0,0 +1,79 @@
+import { apolloClient } from '../boot/apollo-boost';
+import { GET_PROJECTS } from '../graphql/queries';
+import {
+  CREATE_PROJECT_MUTATION,
+  DELETE_PROJECT_MUTATION,
+  FETCH_PROJECT_QUERY,
+  UPDATE_PROJECT_MUTATION
+} from '../graphql/mutations';
+
+export default {
+  get() {
+    apolloClient
+      .query({
+        query: GET_PROJECTS
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  create(payload) {
+    apolloClient
+      .mutate({
+        mutation: CREATE_PROJECT_MUTATION,
+        variables: payload
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  read(payload) {
+    apolloClient
+      .mutate({
+        mutation: FETCH_PROJECT_QUERY,
+        variables: {
+          id: payload.id
+        }
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  update(payload) {
+    apolloClient
+      .mutate({
+        mutation: UPDATE_PROJECT_MUTATION,
+        variables: payload
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  delete(payload) {
+    apolloClient
+      .mutate({
+        mutation: DELETE_PROJECT_MUTATION,
+        variables: {
+          id: payload.id
+        }
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+};
