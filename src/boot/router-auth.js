c277ed2905133a776be9072f83299214dac386ce
mlab817 <mlab817@gmail.com>
2020-05-08 09:37:29 +0800

    added async getRole function to ensure that token and role is loaded before route is entered on refresh

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,65 +1,85 @@
 import { LocalStorage, Dialog } from 'quasar';
 
-export default async ({ router, store }) => {
-  router.beforeEach((to, from, next) => {
-    const token = LocalStorage.getItem('token');
-    let role = '';
-    if (!store.state.auth.loading) {
-      const user = store.getters['auth/user'];
+let user = {}, token = ''
 
-      if (user && user.role) {
-        role = user.role.name;
-      } else {
-        role = '';
-      }
-    } else {
-      role = '';
-    }
+const getRole = async () => {
+	token = await LocalStorage.getItem('token')
+	user = await LocalStorage.getItem('user')
+	const role = user.role ? user.role.name: ''
+	return { token, role }
+}
 
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-      if (!token || token === '') {
-        next({
-          path: '/login',
-          params: { nextUrl: to.fullPath }
-        });
-      } else {
-        if (to.matched.some(record => record.meta.is_admin)) {
-          if (role === 'admin' || role === 'superadmin') {
-            next();
-          } else {
-            Dialog.create({
-              title: 'Restricted',
-              message:
-                'The page you are trying to access is restricted to admins only',
-              cancel: true
-            });
-            next({ name: 'home' });
-          }
-        }
-        if (to.matched.some(record => record.meta.is_encoder)) {
-          if (role === 'encoder') {
-            next();
-          } else {
-            Dialog.create({
-              title: 'Restricted',
-              message:
-                'The page you are trying to access is restricted to encoders only',
-              cancel: true
-            });
-            next({ name: 'home' });
-          }
-        } else {
-          next();
-        }
-      }
-    } else if (to.matched.some(record => record.meta.guest)) {
-      if (!token) {
-        next();
-      } else {
-        next({ name: 'home' });
-      }
-    } else {
-      next();
-    }
-  });
+export default ({ router }) => {
+	getRole().then(({ role, token}) => {
+		// console.log('role: ', role)
+		// console.log('token', token)
+		router.beforeEach((to, from, next) => {
+			if (to.matched.some(record => record.meta.requiresAuth)) {
+				if (!token) {
+					next({
+						path: '/login',
+						params: { nextUrl: to.fullPath }
+					});
+				} else {
+					if (to.matched.some(record => record.meta.isAdmin)) {
+						if (role === 'admin' || role === 'superadmin') {
+							next();
+						} else {
+							Dialog.create({
+								title: 'Restricted',
+								message:
+									'The page you are trying to access is restricted to admins only',
+								cancel: true
+							});
+							// next({ name: 'home' });
+							// redirect to origin page
+							next({ name: 'dashboard' })
+						}
+					}
+					if (to.matched.some(record => record.meta.isEncoder)) {
+						if (role === 'encoder') {
+							next();
+						} else {
+							Dialog.create({
+								title: 'Restricted',
+								message:
+									'The page you are trying to access is restricted to encoders only',
+								cancel: true
+							});
+							// next({ name: 'home' });
+							// redirect to origin page
+							next({ name: 'index-project' })
+						}
+					} else {
+						next();
+					}
+					if (to.matched.some(record => record.meta.isReviewer)) {
+						if (role === 'reviewer') {
+							next();
+						} else {
+							Dialog.create({
+								title: 'Restricted',
+								message:
+									'The page you are trying to access is restricted to reviewers only',
+								cancel: true
+							});
+							// next({ name: 'home' });
+							// redirect to origin page
+							next({ name: 'index-project' })
+						}
+					} else {
+						next();
+					}
+				}
+			} else if (to.matched.some(record => record.meta.guest)) {
+				if (!token) {
+					next();
+				} else {
+					next({ name: 'home' });
+				}
+			} else {
+				next();
+			}
+		});
+	})
 };
