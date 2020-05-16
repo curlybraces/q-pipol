7a43ce5d0934cef9d8d3839abbeb2dfddbf6bd22
mlab817 <mlab817@gmail.com>
2020-05-16 16:40:58 +0800

    finished pagination. this breaks search functionality. endorse project need to be reworked

---
+++ src/modules/projects/components/ProjectPagination.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="row justify-center q-py-md" v-if="maxPages > 1">
+    <q-pagination
+      v-model="page"
+      :max="max"
+      :max-pages="maxPages"
+      :direction-links="true"
+      :boundary-links="true"
+      icon-first="skip_previous"
+      icon-last="skip_next"
+      icon-prev="fast_rewind"
+      icon-next="fast_forward"
+    />
+  </div> 
+</template>
+
+<script>
+	export default {
+		name: 'ProjectPagination',
+		props: ['value','maxPages','max'],
+		computed: {
+			page: {
+				get() {
+					return this.$props.value
+				},
+				set(val) {
+					this.$emit('input',val)
+				}
+			}
+		}
+	}
+</script>
