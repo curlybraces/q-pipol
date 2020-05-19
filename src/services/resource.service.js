ad9887b0a3800bee34018c4040cc2f58945dc5e7
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-05-19 15:35:08 +0800

    feat(refactor): refactored resource into service

---
+++ src/services/resource.service.js
@@ -0,0 +1,40 @@
+import { client } from '@/boot/apollo'
+import { handleResponse, handleError } from '@/utils'
+
+import { FETCH_RESOURCES_QUERY } from '@/graphql/queries'
+import { CREATE_RESOURCE_MUTATION, DELETE_RESOURCE_MUTATION } from '@/graphql/mutations'
+
+export const resourceService = {
+	index() {
+		return client
+			.query({
+				query: FETCH_RESOURCES_QUERY
+			})
+			.then(handleResponse)
+			.catch(handleError)
+	},
+	create({ title, description, url, image_url, document_type }) {
+		return client
+			.mutate({
+				mutation: CREATE_RESOURCE_MUTATION,
+				variables: {
+					title: title,
+          description: description,
+          url: url,
+          image_url: image_url,
+          document_type: document_type
+				}
+			})
+	},
+	delete({ id }) {
+		return client
+			.mutate({
+				mutation: DELETE_RESOURCE_MUTATION,
+				variables: {
+					id: id
+				}
+			})
+			.then(handleResponse)
+			.catch(handleError)
+	}
+}
