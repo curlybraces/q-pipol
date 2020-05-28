a2134e4673863cf6f29d2a6a67256da96466abdf
mlab817 <mlab817@gmail.com>
2020-05-28 17:54:38 +0800

    feat(refactor): made dynamic api endpoint based on env

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,7 @@ export const CONFIG = {
   version: '0.1.0',
   author: 'Mark Lester A. Bolotaolo',
   // graphQLEndpoint: 'http://localhost:8000/graphql'
-  graphQLEndpoint: 'https://wh490179.ispot.cc/graphql'
+  // graphQLEndpoint: 'https://wh490179.ispot.cc/graphql'
+
+  graphQLEndpoint: process.env.DEV ? 'http://localhost:8000/graphql': 'https://wh490179.ispot.cc/graphql'
 };
