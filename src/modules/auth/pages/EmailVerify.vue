2bfeaba3990e0c8eef8aa87af5839118c66c7dde
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-06-05 12:36:40 +0800

    fixed issue showing something went wrong when verifying email

--- src/modules/auth/pages/EmailVerify.vue
+++ src/modules/auth/pages/EmailVerify.vue
@@ -1,21 +1,28 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="column q-gutter-y-xl" v-if="success">
-      <div class="text-h3 text-center">
-        You have successfully verified your email.
-      </div>
+    <template v-if="loading">
+      <q-inner-loading :showing="loading">
+        <q-spinner-tail size="50px" color="primary"></q-spinner-tail>
+      </q-inner-loading>
+    </template>
+    <template v-else>
+      <div class="column q-gutter-y-xl" v-if="success">
+        <div class="text-h3 text-center">
+          You have successfully verified your email.
+        </div>
 
-      <div class="row justify-center">
-        <q-btn size="lg" color="primary" label="Login" to="/login"></q-btn>
+        <div class="row justify-center">
+          <q-btn 
+            size="lg" 
+            color="primary" 
+            label="Login" 
+            to="/login"></q-btn>
+        </div>
       </div>
-    </div>
-    <div v-else>
-      <div class="text-h3 text-center">Something went wrong.</div>
-
-      <div class="row justify-center">
-        Contact us
+      <div v-else>
+        <div class="text-h3 text-center">Something went wrong.</div>
       </div>
-    </div>
+    </template>
   </q-page>
 </template>
 
@@ -26,36 +33,49 @@ export default {
   name: 'EmailVerify',
   data() {
     return {
+      loading: true,
       success: false
     };
   },
-  beforeCreate() {
-    console.log('beforeCreate');
-    this.$apollo
-      .mutate({
-        mutation: gql`
-          mutation verifyEmail($token: String!) {
-            verifyEmail(input: { token: $token }) {
-              access_token
-              refresh_token
-              user {
-                id
-                name
-                email
+  created() {
+    this.loading = true;
+
+    const TOKEN = this.$route.query.token ? this.$route.query.token : null
+
+    if (!TOKEN) {
+      this.loading = false;
+      this.success = false;
+      alert('Unable to retrieve token. Please check the email again.')
+    } else {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation verifyEmail($token: String!) {
+              verifyEmail(input: { token: $token }) {
+                access_token
+                refresh_token
+                user {
+                  id
+                  name
+                  email
+                }
               }
             }
+          `,
+          variables: {
+            token: TOKEN
           }
-        `,
-        variables: {
-          token: this.$route.query.token
-        }
-      })
-      .then(({ data }) => {
-        if (data.verifyEmail) {
-          this.success = true;
-        }
-      })
-      .catch(err => console.error(err));
+        })
+        .then(({ data }) => {
+          if (data.verifyEmail) {
+            this.success = true;
+          }
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+          this.loading = false;
+        });
+    }
   }
 };
 </script>
